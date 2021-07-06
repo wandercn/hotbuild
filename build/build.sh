b0f46b772ae7b7a16af4fed92407c3ffb94f00be
lishenmiao <wander@email.cn>
2021-07-06 15:59:48 +0800

    sh

--- build/build.sh
+++ build/build.sh
@@ -4,22 +4,22 @@ echo $lastTag > version.txt
 # Linux
 GO_ENABLED=0 GOOS=linux GOARCH=amd64
 target="hotbuild_${lastTag}_${GOOS}_${GOARCH}"
-go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
+GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
 cd build/
-zip -mr  $target.zip $target 
+# zip -mr  $target.zip $target 
 cd ..
 # macosx
 GO_ENABLED=0 GOOS=darwin GOARCH=amd64
 target="hotbuild_${lastTag}_${GOOS}_${GOARCH}"
-go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
+GO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
 cd build/
-zip -mr  $target.zip $target 
+# zip -mr  $target.zip $target 
 cd ..
 # windows
 GO_ENABLED=0 GOOS=windows GOARCH=amd64
 target="hotbuild_${lastTag}_${GOOS}_${GOARCH}"
-go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
+GO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
 cd build/
-zip -mr  $target.zip $target 
+# zip -mr  $target.zip $target 
 cd ..
 
