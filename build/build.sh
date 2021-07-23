43e0e1a3080625701a6c1950dafedc0b665bd68e
wander <wander@ffactory.org>
2021-07-23 11:24:18 +0800

    Update build.sh
    add build for arm

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 lastTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo $lastTag > version.txt
-# Linux
+# Linux amd64
 GO_ENABLED=0 GOOS=linux GOARCH=amd64
 target="hotbuild_${lastTag}_${GOOS}_${GOARCH}"
 GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
+cd build/
+zip -mr  $target.zip $target 
+cd ..
+# Linux arm
+GO_ENABLED=0 GOOS=linux GOARCH=arm
+target="hotbuild_${lastTag}_${GOOS}_${GOARCH}"
+GO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags="-X 'main.Version=$lastTag'" -o ./build/$target/ 
 cd build/
 zip -mr  $target.zip $target 
 cd ..
