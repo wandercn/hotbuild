fbca7502f0d8f719a9717e910a1be4f248aa06f8
wander <wander@ffactory.org>
2021-09-02 15:56:04 +0800

    fix test

--- tree_test.go
+++ tree_test.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/wandercn/hotbuild/tree"
 )
 
 func Test_Tree(t *testing.T) {
-
 	dir := "./"
 	list := make([]string, 100)
 	excludeDir := []string{"vendor", ".git", "docs"}
 	err := tree.TreeDirs(dir, &list, excludeDir)
 	if err != nil {
-		t.Fatalf("test tree failed: %v", err)
-	} else {
-		for _, d := range list {
-			fmt.Println(d)
-			t.Log(d)
-		}
+		t.Errorf("test tree failed: %v", err)
 	}
 
 }
