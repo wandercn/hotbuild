8692aa4ab6a33567ce7e82000910ccbce5b34ba5
FNC <fncolon@pm.me>
2022-01-07 10:54:46 +0700

    update the rest

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2021 ffactory.org
+   Copyright (c) 2021-present ffactory.org
    hotbuild is licensed under Mulan PSL v2.
    You can use this software according to the terms and conditions of the Mulan PSL v2.
    You may obtain a copy of Mulan PSL v2 at:
